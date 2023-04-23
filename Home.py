8c6a7995011563bc0d09d448551829c9ffe85226
CassDabi <38007056+CassDabii@users.noreply.github.com>
2023-04-23 19:08:04 +0100

    Update Home.py
    fixing the league of legends picture

--- Home.py
+++ Home.py
@@ -1,159 +1,85 @@
 import streamlit as st
-from pathlib import Path 
 from PIL import Image
-import streamlit.components.v1 as components
+from pathlib import Path 
 
-# Path settings
+st.set_page_config(layout="wide")
+
+st.title('Data Science Course (In Quotation Marks)')
 curr_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
-css_file = curr_dir / 'styles' / 'main.css'
-resume_file = curr_dir / 'assets' / 'Maxwell Ogunsanya - CV.pdf'
-avi = curr_dir / 'Images' / 'pfp.png'
-contact_css = curr_dir / 'styles' / 'contactme.css'
+
+lol_loss = curr_dir / 'Images' / 'lolloss.png'
 
 
+tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(["How this is gonna work!","Some Prior Knowledge", "The Fabled Data", "Not The Maths", "The Science?", "Even Deeper Science?"])
 
-# --- GENERAL SETTINGS ---
-PAGE_TITLE = "Portfolio"
-PAGE_ICON = ":robot:"
-NAME = "Maxwell Ogunsanya"
-DESCRIPTION = """
-Computer Science Graduate, creating custom software solutions.
-"""
-EMAIL = "maxwellogunsanya@gmail.com"
-SOCIAL_MEDIA = {
-    "LinkedIn": "https://www.linkedin.com/in/maxwellogunsanya/",
-    "GitHub": "https://github.com/CassDabii",
-}
-PROJECTS = {
-    "✔️ CassaDoc - Makes use of NLP to aid in the generation of design documnets:": "https://cassadoc.streamlit.app/",
-    "⌛ CassaLog - A trading journal that will use ML to give detailed insights into trading habits":"",
-    "⌛ CassaBot - An alternate UI for the Open assistant AI":"",
-}
+with tab1:
+   st.subheader("What are we even doing right now?")
+   st.write("""
+   I'll tell you what we are doing right now (I sound like a parent). In all seriousness, I have a few motivations for creating a “CoUrSe” on data science.
+  - The first is man's best motivator REJECTION. TLDR is that I’ve been rejected from my dream job at the BBC and I’ve decided that I have to go back to the basics and there is no better way of cementing your knowledge than teaching someone.
+  - The second is that I have a younger cousin that looks up to me and depending on when you are reading this he will be going to university in September 2023 I would like to make something he can read in the way that we would speak to each other to make it a lot more digestible
+  - The third is that I am unemployed what else am I supposed to do? It's either this or playing League of Legends and I am on a losing streak right now soooo….
+   
+   """)
+   
+   image = Image.open(lol_loss)
+   col1, col2, col3 = st.columns(3)
 
+   with col1:
+    st.write(' ')
 
-st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
+   with col2:
+    st.image(image, caption='Melancholy',width=450)
 
-
-# --- LOAD CSS, PDF & PROFIL PIC ---
-with open(css_file) as f:
-    st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
-with open(resume_file, "rb") as pdf_file:
-    PDFbyte = pdf_file.read()
-avi = Image.open(avi)
+   with col3:
+    st.write(' ')
+   
+   st.divider()
 
 
-# --- HERO SECTION ---
-col1, col2 = st.columns(2, gap="small")
-with col1:
-    st.image(avi, width=230)
-with col2:
-    st.title(NAME)
-    st.write(DESCRIPTION)
-    st.download_button(
-        label=" 📄 Download Resume",
-        data=PDFbyte,
-        file_name=resume_file.name,
-        mime="application/octet-stream",
-    )
-    st.write("📫", EMAIL)
+   st.subheader("You haven't answered how this is gonna work?")
+   st.write("""
+   😐 Okay, I will try to explain core concepts of data science and machine learning. There will be many chunks of code which I will explain in-depth and elaborate on why it is done in that way.
+   Also at the end of each section, I will give questions that when answered will encompass all we have learnt these questions can be used in an active recall and spaced repition method to make sure it stays in the noggin.
+   """)
 
+   st.divider() 
 
-# --- SOCIAL LINKS ---
-st.write('\n')
-cols = st.columns(len(SOCIAL_MEDIA))
-for index, (platform, link) in enumerate(SOCIAL_MEDIA.items()):
-    cols[index].write(f"[{platform}]({link})")
+   st.subheader("What is the active recall active recall and spaced repition method?")
+   st.write("""This is when you actively try and retrive information from memory. How it will integrate into this course when answering the end questions for each lesson in regular intervals of a week or some other period
+               it will reinforce your understanding of the material and allow for the information to be actively reaclled. The quesions will not be full blown exam questions, rather questions you would ask yourself when trying to answer the whole question. Take this for example:
+            """)
+   st.code("""
+        
+        Big question:
+        How does a bicycle work?
 
+        Smaller questions:
 
+        - What are the parts of a bicycle? \n
+        - How do the wheels of a bicycle turn? \n
+        - How does a person make a bicycle move forward?
+   """)
+   st.divider()
 
-# --- SKILLS ---
-st.write('\n')
-st.subheader("Hard Skills")
-st.write(
-    """
-- 👩‍💻 Programming: Python, SQL, Java
-- 📊 Data Visulization: PowerBi, MS Excel, Plotly
-- 📚 Modeling: Logistic regression, linear regression, decition trees
-- 🗄️ Databases: Postgres, MongoDB, MySQL, NoSQL
-"""
-)
+   st.subheader("Are you going to do semi-comedic yet holisticly cringe cutaways during the course?")
+   def rambling():
+      st.balloons()
+      
 
+   st.button(label='Click Here To Find Out', on_click=rambling)
 
-# --- WORK HISTORY ---
-st.write('\n')
-st.subheader("Work History")
-st.write("---")
+with tab2:
+   st.header("Some Prior Knowledge")
 
-# --- JOB 1
-st.write("👨‍💻", "**Founder | CassaCorp**")
-st.write("11/2022 - Present")
-st.write(
-    """
-- ► Created 3 successful software products within the first year of operations, resulting in an £1000 investment by effectively managing all aspects of software
-development, from planning to deployment.
-- ► Reduced project development time by 30% by implementing revised agile methodologies for individual projects, resulting in faster time-to-market and
-increased customer satisfaction.
-- ► Developed a strategic plan that resulted in a 50% increase in client base and led to the expansion of the company's services by leveraging networking and
-marketing skills to establish new business relationships and promote the company's services.
-"""
-)
+with tab3:
+   st.header("The Fabled Data")
 
-# --- JOB 2
-st.write('\n')
-st.write("🥡", "**Runner | THE RIVER CAFE**")
-st.write("07/2021 - 01/2022")
-st.write(
-    """
-- ► Achieved an average rating of 4.8 out of 5 stars on customer reviews by providing exceptional service to guests.
-- ► Increased table turnover rate by 20% by implementing efficient table clearance and ordering processes, resulting in increased revenue for the restaurant.
-- ► Trained and mentored new staff members, resulting in improved overall service quality and a more cohesive team environment which netted me employee
-awards on two separate occasions.
-"""
-)
+with tab4:
+   st.header("Not The Maths")
 
-# --- JOB 3
-st.write('\n')
-st.write("💻", "**Junior Web Developer | Start Smart London**")
-st.write("06/2019 - 03/2020")
-st.write(
-    """
-- ► Increased website traffic by 50% and reduced bounce rate by 25% by implementing a comprehensive redesign that prioritized user experience and consistent
-branding.
-- ► Incorporated relevant resources and updated content regularly, resulting in a 40% increase in student engagement and a 90% satisfaction rate among
-students and faculty.
-- ► Developed and implemented a hands-on approach to learning HTML, CSS, and JavaScript, resulting in a 40% improvement in student performance and
-positive feedback from supervisors.
-"""
-)
+with tab5:
+   st.header("The Science?")
 
-
-# --- Projects ---
-st.write('\n')
-st.subheader("CassaCorp Catalogue")
-st.write("---")
-for project, link in PROJECTS.items():
-    st.write(f"[{project}]({link})")
-
-
-
-st.subheader(" Contact Me")
-
-
-contact_form = """
-        <form action="https://formsubmit.co/maxwellogunsanya@gmail.com" method="POST">
-            <input type="hidden" name="_captcha" value="false">
-            <input type="text" name="name" placeholder="Your name" required>
-            <input type="email" name="email" placeholder="Your email" required>
-            <textarea name="message" placeholder="Your message here"></textarea>
-            <button type="submit">Send</button>
-        </form>
-        """
-
-st.markdown(contact_form, unsafe_allow_html=True)
-
-
-# Use Local CSS File
-
-with open(contact_css) as f:
-    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-
+with tab6:
+   st.header("Even Deeper Science?")
